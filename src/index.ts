dad06882b2112b23ebe80eeda4f5e7ff34637b99
edwardzcn-decade <edwardzcn98@gmail.com>
2025-11-24 00:32:18 +0800

    feat(src): add hn api call

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,35 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+const BASE_URL = "https://hacker-news.firebaseio.com/v0/";
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+		switch(url.pathname) {
+			case "/":
+				return new Response('Hello World!');
+			case "/about":
+				return new Response('About: This is a haker news worker.');
+			case "/blog":
+				const blogUrl = new URL("https://edwardzcn.me");
+				return fetch(blogUrl.toString());
+		}
+		// Forward Hacker News API. Fetch item by id
+		const match = url.pathname.match(/^\/item\/(\d+)$/);
+		if (match) {
+			const itemId = match[1].trim();
+			console.log("Fetching item id:", itemId);
+			const url = new URL(`item/${itemId}.json`, BASE_URL);
+			url.searchParams.append("print", "pretty");
+			console.log("Full URL string:", url.toString());
+			const response = await fetch(url.toString(), {
+				headers: {
+					"User-Agent": "Cloudflare Worker - Hacker News API Proxy",
+					"Accept": "application/json",
+				},
+			});
+			return response;
+		}
+		return new Response('Not Found', { status: 404 });
 	},
 } satisfies ExportedHandler<Env>;
